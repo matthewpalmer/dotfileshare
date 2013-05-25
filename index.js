8af53b5c5adac4b327fc466e7c98b2a5fc880e6e
Matthew Palmer <matt@palmer.im>
2013-05-25 10:11:55 +1000

    added sample curl commands

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var fs = require('fs');
 var privateKey = fs.readFileSync('privatekey.pem').toString();
 var certificate = fs.readFileSync('certificate.pem').toString();
 
-
 var restify = require('restify');
+
+//My modules
+var userModule = require('./userModule'),
+    fileModule = require('./fileModule');
 
 var server = restify.createServer({
   certificate: certificate,
@@ -14,6 +17,21 @@ var server = restify.createServer({
 
 server.get('/', function(req, res) {
   res.end('hey');
+});
+
+//User
+server.get('/user/:id', function(req, res) {
+  userModule.getUser(id, function(err, item) {
+    if (err) {
+      res.writeHead(404);
+      res.end();
+    } else {
+      res.writeHead(200);
+      console.log('found ', item);
+      res.write(item);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, function() {
