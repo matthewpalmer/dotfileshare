2f9f32df18c5f64b5e29fd58d261e7d7a2e65580
Matthew Palmer <matt@palmer.im>
2013-05-25 10:28:17 +1000

    added new user, find user routes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var privateKey = fs.readFileSync('privatekey.pem').toString();
 var certificate = fs.readFileSync('certificate.pem').toString();
 
-var restify = require('restify');
+var restify = require('restify'),
+    postdata = require('postdata');
 
 //My modules
 var userModule = require('./userModule'),
@@ -16,24 +17,38 @@ var server = restify.createServer({
 });
 
 server.get('/', function(req, res) {
-  res.end('hey');
+  res.end('Please see the API docs.');
 });
 
 //User
-server.get('/user/:id', function(req, res) {
-  userModule.getUser(id, function(err, item) {
+server.get('/user/:id', function(req, res) { 
+  userModule.getUser(req.params.id, function(err, item) {
     if (err) {
       res.writeHead(404);
       res.end();
     } else {
       res.writeHead(200);
-      console.log('found ', item);
-      res.write(item);
+      res.write(item.toString());
       res.end();
     }
-  });
+  }); 
 });
 
+server.post('/user', function(req, res) {
+  postdata(req, res, function(err, data) {
+    var name = data.name;
+    userModule.createUser(name, function(err, item) {
+      if (err) {
+        res.writeHead(404);
+        res.end();
+      } else {
+        res.writeHead(201);
+        res.write(item.toString());
+        res.end();
+      }
+    });
+  });
+});
 server.listen(3000, function() {
   console.log('Server listening on port 3000');
 });
